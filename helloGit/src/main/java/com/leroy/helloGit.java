050fa65df3acbb2ee61b4f0e3989383f5f39bd74
xiaoleroy <xiaoleroy@163.com>
2021-01-16 14:51:14 +0800

    第一次添加文件

---
+++ helloGit/src/main/java/com/leroy/helloGit.java
@@ -0,0 +1,11 @@
+package com.leroy;
+
+/**
+ * @author shkstart
+ * @create 2021-01-16 14:44
+ */
+public class helloGit {
+    public static void main(String[] args) {
+        System.out.println("hello word！ 001");
+    }
+}
